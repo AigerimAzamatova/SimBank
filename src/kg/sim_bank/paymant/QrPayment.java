d256c470ccda51a9a898d478f4eb80575033fddc
Искандер Каримов <iskanderkarimov@MacBook-Air-Iskander-2.local>
2026-01-13 23:26:16 +0600

    added sout and VisaPayment class

--- src/kg/sim_bank/paymant/QrPayment.java
+++ src/kg/sim_bank/paymant/QrPayment.java
@@ -3,5 +3,6 @@ package kg.sim_bank.paymant;
 public class QrPayment {
     public static void main(String[] args) {
         System.out.println("Qr payment");
+        System.out.println("Hello World");
      }
 }
