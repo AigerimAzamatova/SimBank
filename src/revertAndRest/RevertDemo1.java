c5c9aba62c9e3522eaa77acb033a77056f075f2a
AigerimAzamatova <azamatovvva@gmail.com>
2026-01-19 23:45:52 +0600

    Revert "My name"
    This reverts commit ea994b5e880f22bba0af683464c4ae8b221df07e.

--- src/revertAndRest/RevertDemo1.java
+++ src/revertAndRest/RevertDemo1.java
@@ -3,6 +3,5 @@ package revertAndRest;
 public class RevertDemo1 {
     public static void main(String[] args) {
         System.out.println("First one");
-        System.out.println(My names Aigerim);
     }
 }
